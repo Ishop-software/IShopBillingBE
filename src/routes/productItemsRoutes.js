a54dda9621873381c5fe87ed62936c14e411d643
Kavitha <kavithaishop@gmail.com>
2024-08-06 15:21:52 +0530

    ProductItemRouter And UserRouter File Some APIs Added

--- src/routes/productItemsRoutes.js
+++ src/routes/productItemsRoutes.js
@@ -4,15 +4,78 @@ import { ProductItem } from "../models/ProductItemModel.js";
 
 const router = express.Router();
 
-router.post("/api/addProductItem", async (req, res) => {
+// Add Product Items
+router.post("/api/productitems/addProductItem", async (req, res) => {
     try {
         const prdouctDataList = req.body;
         const randomId = uuidv4();
         prdouctDataList["productItemId"] = randomId;
         const CreateProductData = await ProductItem.create(prdouctDataList);
-        return res.status(200).json({ success: false, message: "Product Item Created Successfully!", data: CreateProductData });
+        return res.status(200).json({ success: true, message: "Product Item Created Successfully!", data: CreateProductData });
     } catch (error) {
-        return res.json({ success: false, message: error.message });
+        return res.status(500).json({ success: false, message: "Internal Server Error!" });
+    }
+});
+
+// Get All Product Item List
+router.get("/api/productitems/getAllProductItems", async (req, res) => {
+    try {
+        const findAllProductItems = await ProductItem.find({});
+        if (findAllProductItems) {
+            return res.status(200).json({ success: true, message: findAllProductItems });
+        } else {
+            return res.status(404).json({ success: true, message: "Product Item List Is Not Found!" });
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Internal Server Error!" });
+    }
+});
+
+// Get Product Item
+router.get("/api/productitems/getProductItem", async (req, res) => {
+    try {
+        const prdouctDataList = req.body;
+        const getProductId =  prdouctDataList.productItemId;
+        const findProductId = await ProductItem.findOne({ productItemId: getProductId });
+        if (findProductId) {
+            return res.status(200).json({ success: true, message: findProductId });
+        } else {
+            return res.status(404).json({ success: true, message: "Product Item Data Id Is Not Found!" });
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Internal Server Error!" });
+    }
+});
+
+// Update Product Item 
+router.put("/api/productitems/updateProductItem", async (req, res) => {
+    try {
+        const prdouctDataList = req.body;
+        const getProductId = prdouctDataList.productItemId;
+        const updateProduct = await ProductItem.findOneAndUpdate({ productItemId: getProductId }, { $set: prdouctDataList });
+        if (updateProduct) {
+            return res.status(200).json({ success: true, message: "Product Item Updated Successfully!" });
+        } else {
+            return res.status(404).json({ success: false, message: "Product Item Not Found!" });
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
+});
+
+// Delete Product Item
+router.delete("/api/productitems/deleteProductItem", async (req, res) => {
+    try {
+        const prdouctDataList = req.body;
+        const getProductId = prdouctDataList.productItemId; 
+        const delProductItem = await ProductItem.deleteOne({ productItemId: getProductId });
+        if (delProductItem.deletedCount ===1) {
+            return res.status(200).json({ success: true, message: "Product Item Id Deleted Successfully!" })
+        } else {
+            return res.status(404).json({ success: false, message: "Product Item Id Is Not Found!" });
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: "Internal Server Error!" });
     }
 });
 
