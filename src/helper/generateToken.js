f6c7817c357f2a3862615668e76e571663bcf50e
Sundaresh-ishop <sundareshishop@gmail.com>
2024-08-23 00:38:25 +0530

    function for get userId from token

--- src/helper/generateToken.js
+++ src/helper/generateToken.js
@@ -1,15 +1,14 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (data) => {
-    const secretKey = "kavithatp98@gmail.com";
     const hourseExpiresIn = "10h";
-    const token = jwt.sign({data}, secretKey, { expiresIn: hourseExpiresIn });
+    const token = jwt.sign({data}, process.env.SECRET_KEY, { expiresIn: hourseExpiresIn });
     return token;
 };
 
 export const tokenValidation = (token) => {
     try {
-        return jwt.verify(token, "keytest");
+        return jwt.verify(token, process.env.SECRET_KEY);
     } catch (error) {
         return false;
     }
@@ -19,3 +18,8 @@ export const validUser = (req, res) => {
     const token = req.header("Authorization");
     return res({ token });
 };
+
+export const getUserIdFromToken = (token) => {
+    const userId = jwt.decode(token,process.env.SECRET_KEY);
+    return userId.data.userId;
+}
